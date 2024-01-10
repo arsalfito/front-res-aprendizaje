a5fda0f781ce0f499035463a2f1272127af642f0
Alfonso Rodriguez <arsalfito@gmail.com>
2024-01-09 19:22:54 -0500

    Incremento rutas, login y menu

--- src/app/aprendizaje/pages/salir/salir.component.ts
+++ src/app/aprendizaje/pages/salir/salir.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AppSharedService } from 'src/app/services/app-shared-service';
 
 @Component({
   selector: 'app-salir',
@@ -8,13 +9,16 @@ import { Router } from '@angular/router';
 })
 export class SalirComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+    private appSharedService: AppSharedService) { }
 
   ngOnInit(): void {
   }
 
   logout(){
     console.log('logout');
+
+    this.appSharedService.actualizarInformacion(null);
     sessionStorage.setItem('logged', '0');
     this.router.navigate(['./login/login']);
   }
--- src/app/shared/components/side-menu/side-menu.component.ts
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { AppSharedService } from 'src/app/services/app-shared-service';
 
 interface MenuItem{
   title: string;
@@ -12,16 +13,28 @@ interface MenuItem{
 })
 export class SideMenuComponent implements OnInit {
 
-  public reactiveMenu: MenuItem[] = [
+  public reactiveMenu: MenuItem[] = [];
+
+  
+   /*public reactiveMenu: MenuItem[] = [
     {title: 'Calificaciones', route: './aprendizaje/calificaciones'},
     {title: 'Resultados', route: './aprendizaje/resultados'},
     {title: 'Criterios de evaluación', route: './aprendizaje/criterios'},
     {title: 'Cerrar sesión', route: './aprendizaje/salir'},
-  ];
+  ];*/
+   
 
-  constructor() { }
+  constructor(private appSharedService: AppSharedService) { }
 
   ngOnInit(): void {
+    this.appSharedService.informacion$.subscribe((nuevaInformacion) => {
+      if(nuevaInformacion!=null){
+        console.log(nuevaInformacion);
+        this.reactiveMenu.push(nuevaInformacion!);
+      }else{
+        this.reactiveMenu = [];
+      }     
+    });
   }
 
 }
